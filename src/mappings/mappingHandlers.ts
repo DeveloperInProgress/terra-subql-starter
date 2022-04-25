7deb037468562b8db22f312a3f82a22a505a03fe
DeveloperInprogress <velnaveen99@gmail.com>
2022-04-25 01:56:01 +0000

    add templates

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,58 +1,38 @@
-import { Block, Message, Transaction, TransferEvent } from "../types";
 import {
   TerraEvent,
   TerraBlock,
   TerraMessage,
   TerraTransaction,
 } from "@subql/types-terra";
-import { MsgExecuteContract } from "@terra-money/terra.js";
-
-export async function handleBlock(block: TerraBlock): Promise<void> {
-  const record = new Block(block.block.block_id.hash);
-  record.height = BigInt(block.block.block.header.height);
-  await record.save();
-}
-
-export async function handleTransaction(tx: TerraTransaction): Promise<void> {
-  const record = new Transaction(tx.tx.txhash);
-  record.blockHeight = BigInt(tx.block.block.block.header.height);
-  record.timestamp = tx.tx.timestamp;
-  await record.save();
-}
-
-export async function handleMessage(
-  msg: TerraMessage<MsgExecuteContract>
-): Promise<void> {
-  const record = new Message(`${msg.tx.tx.txhash}-${msg.idx}`);
-  record.blockHeight = BigInt(msg.block.block.block.header.height);
-  record.txHash = msg.tx.tx.txhash;
-  record.contract = msg.msg.toData().contract;
-  record.sender = msg.msg.toData().sender;
-  record.executeMsg = JSON.stringify(msg.msg.toData().execute_msg);
-  await record.save();
-}
+import { MsgExecuteContract,MsgInstantiateContract } from "@terra-money/terra.js";
+import { createNFTContractDatasource, NftContractInstantiation, SendNFT } from "../types";
 
 export async function handleEvent(
-  event: TerraEvent<MsgExecuteContract>
+  event: TerraEvent<MsgInstantiateContract>
 ): Promise<void> {
-  const record = new TransferEvent(
-    `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`
-  );
-  record.blockHeight = BigInt(event.block.block.block.header.height);
-  record.txHash = event.tx.tx.txhash;
-  for (const attr of event.event.attributes) {
-    switch (attr.key) {
-      case "sender":
-        record.sender = attr.value;
-        break;
-      case "recipient":
-        record.recipient = attr.value;
-        break;
-      case "amount":
-        record.amount = attr.value;
-        break;
-      default:
+  const initMsg: any = event.msg.msg.init_msg;
+  if(initMsg.name && initMsg.minter && initMsg.symbol) {
+    const entity = new NftContractInstantiation(`${event.tx.tx.txhash}-${event.idx}`)
+    entity.name = initMsg.name;
+    entity.symbol = initMsg.symbol;
+    entity.creator = event.msg.msg.sender;
+    entity.codeId = event.msg.msg.code_id;
+    for(const e of event.event.attributes){
+      if(e.key === 'contract_address') {
+        entity.contractAddress = e.value;
+      }
     }
+    await entity.save()
+    await createNFTContractDatasource({value: {contract: entity.contractAddress}})
   }
-  await record.save();
+} 
+
+export async function handleSendNFT(message: TerraMessage<MsgExecuteContract>) {
+  const executeMsg: any = message.msg.execute_msg;
+  const entity = new SendNFT(`${message.tx.tx.txhash}-${message.idx}`)
+  entity.seller = message.msg.sender;
+  entity.recipient = executeMsg.send_nft.contract;
+  entity.nftContract = message.msg.contract;
+  entity.tokenId = executeMsg.send_nft.token_id;
+  await entity.save();
 }
